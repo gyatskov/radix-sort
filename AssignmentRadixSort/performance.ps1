f3aa5b2b034d635e130ebd65172606f9f3faaf90
Nico Mürdter <nicomuerdter@gmail.com>
2016-04-20 11:05:19 +0200

    Added performancetests

---
+++ AssignmentRadixSort/performance.ps1
@@ -0,0 +1,7 @@
+﻿write "Starting Testloop..."
+
+for ($i=25; $i -gt 0; $i--) {
+    $p = [math]::Pow(2,$i)
+    write "..\build\Debug\Assignment.exe --num-elements $p --perf-csv-to-stdout > ..\Performance\input_test_2_$i.txt 2>&1"
+    ..\build\Debug\Assignment.exe --num-elements $p --perf-csv-to-stdout > ..\Performance\input_test_2_$i.txt 2>&1
+}
