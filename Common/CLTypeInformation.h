0774714299de1acdaf5cd1fb36c7dcc3ec1b13cd
Gennadij Yatskov <vokstay@gmx.de>
2020-10-25 19:55:12 +0100

    Simplify CLTypeInformation

--- Common/CLTypeInformation.h
+++ Common/CLTypeInformation.h
@@ -1,53 +1,43 @@
 #pragma once
 
+#include <cstdint>
+
 template <typename T>
 struct TypeNameString
 {};
 
 template<>
 struct TypeNameString<int16_t> {
-	static const char* const stdint_name;
-    static const char* const open_cl_name;
+	inline static constexpr const char* const stdint_name  = "int16_t";
+    inline static constexpr const char* const open_cl_name = "short";
 };
-const char* const TypeNameString<int16_t>::stdint_name  = "int16_t";
-const char* const TypeNameString<int16_t>::open_cl_name = "short";
 
 template<>
 struct TypeNameString<uint16_t> {
-	static const char* const stdint_name;
-    static const char* const open_cl_name;
+	inline static constexpr const char* const stdint_name= "uint16_t";
+    inline static constexpr const char* const open_cl_name= "unsigned short";
 };
-const char* const TypeNameString<uint16_t>::stdint_name  = "uint16_t";
-const char* const TypeNameString<uint16_t>::open_cl_name = "unsigned short";
 
 template<>
 struct TypeNameString<int32_t> {
-	static const char* const stdint_name;
-    static const char* const open_cl_name;
+	inline static constexpr const char* const stdint_name= "int32_t";
+    inline static constexpr const char* const open_cl_name= "int";
 };
-const char* const TypeNameString<int32_t>::stdint_name  = "int32_t";
-const char* const TypeNameString<int32_t>::open_cl_name = "int";
 
 template<>
 struct TypeNameString<uint32_t> {
-	static const char* const stdint_name;
-    static const char* const open_cl_name;
+	inline static constexpr const char* const stdint_name= "uint32_t";
+    inline static constexpr const char* const open_cl_name= "unsigned int";
 };
-const char* const TypeNameString<uint32_t>::stdint_name  = "uint32_t";
-const char* const TypeNameString<uint32_t>::open_cl_name = "unsigned int";
 
 template<>
 struct TypeNameString<int64_t> {
-	static const char* const stdint_name;
-    static const char* const open_cl_name;
+	inline static constexpr const char* const stdint_name= "int64_t";
+    inline static constexpr const char* const open_cl_name= "long";
 };
-const char* const TypeNameString<int64_t>::stdint_name  = "int64_t";
-const char* const TypeNameString<int64_t>::open_cl_name = "long";
 
 template<>
 struct TypeNameString<uint64_t> {
-	static const char* const stdint_name;
-    static const char* const open_cl_name;
+	inline static constexpr const char* const stdint_name= "uint64_t";
+    inline static constexpr const char* const open_cl_name= "unsigned long";
 };
-const char* const TypeNameString<uint64_t>::stdint_name  = "uint64_t";
-const char* const TypeNameString<uint64_t>::open_cl_name = "unsigned long";
