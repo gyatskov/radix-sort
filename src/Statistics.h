e58dd04071a35e484f71fb41bad2aed7ade00a29
Gennadij Sergej Yatskov <vokstay@gmx.de>
2021-04-10 18:34:42 +0200

    Document Statistics struct

--- src/Statistics.h
+++ src/Statistics.h
@@ -5,14 +5,19 @@
 
 struct Statistics
 {
+    // Returns minimum value
     double min{std::numeric_limits<decltype(min)>::infinity()};
+    // Returns maximum value
     double max{-std::numeric_limits<decltype(max)>::infinity()};
+    // Returns average value
     double avg{0.0};
+    // Returns sum of values
     double sum{0.0};
 
     /** Sample count **/
     std::size_t n{0U};
 
+    /** Adds value to statistic **/
     void update(double value) {
         n++;
         sum += value;
